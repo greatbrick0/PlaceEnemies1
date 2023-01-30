32105fd5aba343218d7bc27937dbdbce8f0a7495
Spencer <speharley@gmail.com>
2023-01-30 15:51:22 -0500

    Worked On Placing UI
    Functionality not included, needs some more development time.

--- Assets/Scripts/EnemyPanelScript.cs
+++ Assets/Scripts/EnemyPanelScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class EnemyPanelScript : MonoBehaviour
+public class EnemyPanelScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
+    [SerializeField]
+    public PlacingManager managerRef;
     [SerializeField]
     public GameObject linkedObject;
     [SerializeField]
@@ -12,4 +15,37 @@ public class EnemyPanelScript : MonoBehaviour
 
     [SerializeField]
     private string objectDescription = "";
+    [SerializeField]
+    private Vector2 hoverOffset = Vector2.right * 30;
+    [SerializeField]
+    public Vector2 verticalOffset = Vector2.zero;
+
+    private float timeHovered = 0.0f;
+    private bool currentlyHovered = false;
+    private bool currentlyDragging = false;
+
+    private void Update()
+    {
+        if (currentlyHovered)
+        {
+            timeHovered += 1.0f * Time.deltaTime;
+        }
+        transform.localPosition = verticalOffset + Vector2.Lerp(Vector2.zero, hoverOffset, Mathf.Min(Mathf.Sqrt(timeHovered * 8), 1));
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        currentlyHovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        currentlyHovered = false;
+        timeHovered = 0.0f;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print("Down");
+    }
 }
