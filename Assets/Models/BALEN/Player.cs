ca4724f5c0a6311d9d7352f92fbf0f52e3c899df
Adam <adam.fifield@ontariotechu.ca>
2023-03-01 16:09:51 -0500

    Improved animator for Balen
    Improved the directional animator script a bit, its better but not perfect and will be improved.

--- Assets/Models/BALEN/Player.cs
+++ Assets/Models/BALEN/Player.cs
@@ -4,44 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
 
- 
-
-  
-
-    private Vector3 moveDirection;
-    Rigidbody cc;
     private Animator animator;
-  
-    void Start()
+    private Rigidbody cc;
+    private float currentXSpeed;
+    private float currentYSpeed;
+    public float turnSpeed = 0.1f;
+    public float speedSmoothTime = 0.1f;
+    public float speedSmoothFactor = 2f;
+
+    private void Start()
     {
-
-        animator = GetComponent<Animator>();
-        
+        animator = GetComponentInChildren<Animator>();
         cc = GetComponent<Rigidbody>();
-   
-
     }
 
-    void Update()
+    private void Update()
     {
-        // Retrieve the rigidbody's global velocity
-        Vector3 velocity = cc.velocity;
+        // Get the local velocity
+        Vector3 localVelocity = transform.InverseTransformDirection(cc.velocity);
 
-        // Set xSpeed and ySpeed based on the global velocity components
-        float xSpeed = velocity.x;
-        float ySpeed = velocity.z;
+        // Set xSpeed and ySpeed based on the local velocity components
+        float xSpeed = localVelocity.x;
+        float ySpeed = localVelocity.z;
+
+        // Smoothly adjust the speed based on the rotation of the model
+        float angle = Vector3.Angle(Vector3.forward, transform.forward);
+        if (Vector3.Dot(transform.right, Vector3.forward) < 0)
+        {
+            angle = 360 - angle;
+        }
+        float adjustedSpeed = Mathf.Lerp(currentYSpeed, currentXSpeed, angle / 90f);
+        currentYSpeed = Mathf.Lerp(currentYSpeed, ySpeed, Time.deltaTime * speedSmoothFactor / speedSmoothTime);
+        currentXSpeed = Mathf.Lerp(currentXSpeed, xSpeed, Time.deltaTime * speedSmoothFactor / speedSmoothTime);
 
         // Set the x and y speed parameters in the animator
         animator.SetFloat("XSpeed", xSpeed);
         animator.SetFloat("YSpeed", ySpeed);
     }
-   
-
-
-
-
-
-
 }
