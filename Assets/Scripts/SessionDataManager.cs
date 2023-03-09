45d78dde88227c2e10829cbdda02337bc01d0f61
Spencer <speharley@gmail.com>
2023-03-09 13:30:40 -0500

    Added Reset Session Function

--- Assets/Scripts/SessionDataManager.cs
+++ Assets/Scripts/SessionDataManager.cs
@@ -7,15 +7,13 @@ public class SessionDataManager
     public static List<Ability> playerLoadOut = new List<Ability>();
     public static int nightNum = 0;
     public static int savedPlayerHealth = 0;
-<<<<<<< HEAD
     public static int currency;
-=======
 
     public static void ResetSession()
     {
         playerLoadOut = new List<Ability>();
         nightNum = 0;
         savedPlayerHealth = 0;
+        currency = 0;
     }
->>>>>>> main
 }
